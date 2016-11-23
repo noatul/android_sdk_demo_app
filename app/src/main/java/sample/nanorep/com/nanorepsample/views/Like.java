e87f7966684369f1f5263958646401ad213ffa81
nanorep <noat@nanorep.com>
2016-11-23 15:14:03 +0200

    custom like view

---
+++ app/src/main/java/sample/nanorep/com/nanorepsample/views/Like.java
@@ -0,0 +1,61 @@
+package sample.nanorep.com.nanorepsample.views;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+
+import nanorep.nanowidget.Components.AbstractViews.NRCustomLikeView;
+import sample.nanorep.com.nanorepsample.R;
+
+/**
+ * Created by noat on 23/11/2016.
+ */
+
+public class Like extends NRCustomLikeView implements View.OnClickListener{
+
+    ImageButton like,dislike;
+
+    public Like(Context context) {
+        super(context);
+        LayoutInflater.from(context).inflate(R.layout.like, this);
+    }
+
+    @Override
+    public void onViewAdded(View child) {
+        super.onViewAdded(child);
+
+        like = (ImageButton) child.findViewById(R.id.likeButton);
+        dislike = (ImageButton) child.findViewById(R.id.dislikeButton);
+
+        like.setOnClickListener(this);
+        dislike.setOnClickListener(this);
+    }
+
+    @Override
+    public void updateLikeButton(boolean isLike) {
+
+    }
+
+    @Override
+    public void resetLikeView() {
+
+    }
+
+    @Override
+    public boolean getLikeSelection() {
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.likeButton:
+                mListener.onLikeClicked(Like.this, null, true);
+                break;
+            case R.id.dislikeButton:
+                break;
+
+        }
+    }
+}
